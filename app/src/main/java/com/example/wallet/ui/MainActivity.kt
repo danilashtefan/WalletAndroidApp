b130b3e621b6c5669ad72dddae1951f11cfaa10f
danilashtefan <danilashtefan@gmail.com>
2021-09-10 16:33:25 +0200

    next

--- app/src/main/java/com/example/wallet/ui/MainActivity.kt
+++ app/src/main/java/com/example/wallet/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wallet.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.wallet.ui.screens.ExpanseCategoriesScreen
 import com.example.wallet.ui.screens.ExpansesScreen
 import com.example.wallet.ui.theme.WalletTheme
 
@@ -10,8 +11,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WalletTheme() {
-                ExpansesScreen()
+            WalletTheme {
+                ExpanseCategoriesScreen()
             }
 
 
