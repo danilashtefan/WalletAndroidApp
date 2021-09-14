0376a24ef5a97b2663e263ca4f8b733c830f4f1e
danilashtefan <danilashtefan@gmail.com>
2021-09-14 13:45:01 +0200

    next

--- app/src/main/java/com/example/wallet/ui/screens/ExpansesScreen.kt
+++ app/src/main/java/com/example/wallet/ui/screens/ExpansesScreen.kt
@@ -1,31 +1,21 @@
 package com.example.wallet.ui.screens
 
-import android.content.Context
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.example.wallet.model.Expanse
 import com.example.wallet.model.viewmodel.ExpansesViewModel
-import com.example.wallet.ui.theme.WalletTheme
 import com.example.wallet.R
 
 @Composable
@@ -35,7 +25,7 @@ fun ExpansesScreen(navHostController: NavHostController) {
     Column() {
         LazyColumn {
             items(expanses) { expanse ->
-                Text(text = expanse.categoryName)
+                ReusableRow(categoryName = expanse.categoryName, date = "Date", location = "Location", amount = expanse.amount)
             }
         }
     }
@@ -45,33 +35,35 @@ fun ExpansesScreen(navHostController: NavHostController) {
 @Composable
 //Parameters: CategoryName, wallet, date, location,amount
 private fun ExpanseRow(categoryName: String, date: String, location: String, amount: Int) {
-    BaseRow(/*TODO*/)
+    ReusableRow(categoryName,date, location, amount  )
 }
 
 @Composable
-private fun BaseRow(/*TODO*/) {
-
+private fun ReusableRow(categoryName: String, date: String, location: String, amount: Int) {
     val amountSign = "$ "
 
+Card() {
+    Row(
+        modifier = Modifier
+            .height(68.dp),
+        verticalAlignment = Alignment.CenterVertically,
 
-    Row( modifier = Modifier
-        .height(68.dp),
-        verticalAlignment =  Alignment.CenterVertically
-    ){
+        ) {
         Spacer(Modifier.width(12.dp))
         Column(Modifier) {
             Image(
-                painter = painterResource(id = R.drawable.groceries ),
+                painter = painterResource(id = R.drawable.groceries),
                 contentDescription = "",
-            modifier = Modifier.size(25.dp))
+                modifier = Modifier.size(25.dp)
+            )
             Spacer(Modifier.width(15.dp))
-            Text(text = "Bills")
+            Text(text = categoryName)
         }
         Spacer(Modifier.weight(0.4f))
         Column(Modifier) {
             Text("Wallet")
-            Text("Date")
-            Text("Location")
+            Text(date)
+            Text(location)
 
         }
         Spacer(Modifier.weight(0.3f))
@@ -98,27 +90,15 @@ private fun BaseRow(/*TODO*/) {
             )
         }
 
-
     }
-//    Card(
-//        shape = CircleShape,
-//        border = BorderStroke(width = 2.dp, color = Color.Cyan),
-//        modifier = Modifier.padding(16.dp),
-//        elevation = 4.dp
-//    ){
-//        Image(painter = painterResource(id = R.drawable.groceries),
-//            contentDescription ="",
-//        modifier = Modifier.size(72.dp),
-//        contentScale = ContentScale.Crop)
-//
-//    }
-
+    Spacer(Modifier.width(30.dp))
+}
 }
 
 @Preview
 @Composable
 fun ComposablePreview() {
-    BaseRow()
+    ReusableRow("","","",1)
 }
 
 
