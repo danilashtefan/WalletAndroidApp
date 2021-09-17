412b503fdcfeed37e14c202a1267daa9b9c28df4
danilashtefan <danilashtefan@gmail.com>
2021-09-17 11:22:04 +0200

    Date picker creation attempts

--- app/src/main/java/com/example/wallet/ui/screens/ExpansesScreen.kt
+++ app/src/main/java/com/example/wallet/ui/screens/ExpansesScreen.kt
@@ -1,16 +1,15 @@
 package com.example.wallet.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,22 +38,33 @@ fun ExpansesScreen(navHostController: NavHostController) {
 
 @Composable
 fun TransactionListSection(){
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(modifier = Modifier.padding(5.dp))
-        Row() {
-            Image(
-                painter = painterResource(id = R.drawable.wallet_no_background_cropped),
-                contentDescription = "",
-                modifier = Modifier
-                    .size(120.dp)
-                    .weight(1f)
+    Column() {
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.padding(5.dp))
+            Row() {
+                Image(
+                    painter = painterResource(id = R.drawable.wallet_no_background_cropped),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(120.dp)
+                        .weight(1f)
+                )
+            }
+            Spacer(modifier = Modifier.padding(5.dp))
+            Text(
+                text = "Transaction list",
+                color = Color.White,
+                style = MaterialTheme.typography.h6
             )
         }
-        Spacer(modifier = Modifier.padding(5.dp))
-        Text(text = "Transaction list", color = Color.White)
-
+        Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.padding(start = 25.dp)) {
+            Text(text = "Period: ",style =MaterialTheme.typography.h6, color = Color.White )
+        }
+        Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.padding(start = 25.dp)) {
+            Text(text = "Total expanses:",style =MaterialTheme.typography.h6, color = Color.White )
+        }
     }
-
 }
 
 
@@ -74,61 +84,82 @@ private fun ExpanseRow(categoryName: String, date: String, location: String, amo
     ReusableRow(categoryName,date, location, amount  )
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun ReusableRow(categoryName: String, date: String, location: String, amount: Int) {
     val amountSign = "$ "
 
-Card(backgroundColor = Color.White,
-modifier = Modifier.padding(6.dp)) {
-    Row(
-        modifier = Modifier
-            .height(68.dp),
-        verticalAlignment = Alignment.CenterVertically,
+    Card(
+        backgroundColor = Color.White,
+        modifier = Modifier.padding(6.dp)
+    ) {
+        var expanded by remember { mutableStateOf(false) }
+        Column(Modifier.clickable{expanded= !expanded}) {
 
-        ) {
-        Spacer(Modifier.width(12.dp))
-        Column(Modifier) {
-            Image(
-                painter = painterResource(id = R.drawable.groceries),
-                contentDescription = "",
-                modifier = Modifier.size(25.dp)
-            )
-            Spacer(Modifier.width(15.dp))
-            Text(text = categoryName)
-        }
-        Spacer(Modifier.weight(0.4f))
-        Column(Modifier) {
-            Text("Wallet")
-            Text("Date")
-            Text("Location")
-        }
-        Spacer(Modifier.weight(0.3f))
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = amountSign,
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
-            Text(
-                text = "121",
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
-        }
-        Spacer(Modifier.width(16.dp))
-        IconButton({ println("Pressed") }) {
-            Icon(
-                imageVector = Icons.Filled.ChevronRight,
-                contentDescription = null,
+            Row(
                 modifier = Modifier
-                    .padding(end = 12.dp)
-                    .size(24.dp),
-            )
-        }
+                    .height(68.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                ) {
+                Spacer(Modifier.width(12.dp))
+                Column(Modifier) {
+                    CategoryImage()
+                    Spacer(Modifier.width(15.dp))
+                    Text(text = categoryName)
+                }
+                Spacer(Modifier.weight(0.4f))
+                Column(Modifier) {
+                    Text("Wallet")
+                    Text("Date")
+                    Text("Location")
+                }
+                Spacer(Modifier.weight(0.3f))
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = amountSign,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                    Text(
+                        text = "121",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+                Spacer(Modifier.width(16.dp))
+                IconButton({ println("Pressed") }) {
+                    Icon(
+                        imageVector = Icons.Filled.ChevronRight,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(end = 12.dp)
+                            .size(24.dp),
+                    )
+                }
 
+            }
+            //Spacer(Modifier.width(30.dp))
+            AnimatedVisibility(visible = expanded) {
+                Text(
+                    text = "Animation",
+                    style = MaterialTheme.typography.body1,
+
+                    )
+            }
+
+
+        }
     }
-    Spacer(Modifier.width(30.dp))
+
 }
+
+@Composable
+fun CategoryImage() {
+    Image(
+        painter = painterResource(id = R.drawable.groceries),
+        contentDescription = "",
+        modifier = Modifier.size(25.dp)
+    )
 }
 
 @Preview
