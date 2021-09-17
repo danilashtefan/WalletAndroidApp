5775a31c107d490ca0b9c512f8a16e65f651a98c
danilashtefan <danilashtefan@gmail.com>
2021-09-17 19:19:51 +0200

    next

--- app/src/main/java/com/example/wallet/ui/screens/AddTransactionScreen.kt
+++ app/src/main/java/com/example/wallet/ui/screens/AddTransactionScreen.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +23,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.wallet.R
 import com.example.wallet.model.viewmodel.ExpanseCategoriesViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun AddTransactionScreen() {
@@ -38,10 +44,36 @@ fun AddTransactionScreen() {
             border = BorderStroke(width = 2.dp, color = Color.White),
             modifier = Modifier.size(150.dp),
             elevation = 5.dp
-         ){
+         ) {
             CategoryImage()
-
          }
       }
+      InfoRow(100,"Category")
+      InfoRow(20,"Date")
+      InfoRow(20,"Notes")
+      InfoRow(20,"Location")
+
+   }
+   }
+
+@Composable
+private fun InfoRow(padding:Int, labelText:String) {
+   Row(
+      modifier = Modifier
+         .padding(top = padding.dp)
+         .fillMaxWidth(),
+      verticalAlignment = Alignment.CenterVertically,
+      horizontalArrangement = Arrangement.Center
+   ) {
+      var text by rememberSaveable { mutableStateOf("") }
+      TextField(
+         value = text,
+         onValueChange = {
+            text = it
+         },
+         label = { Text(labelText) }
+      )
    }
 }
+
+
