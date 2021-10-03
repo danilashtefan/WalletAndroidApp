c1a51e8b2c4fa44c471cf1b9452aa5a251889264
danilashtefan <danilashtefan@gmail.com>
2021-10-03 18:11:47 +0200

    next

---
+++ app/src/main/java/com/example/wallet/model/response/transactions/AllTransactionWalletsResponse.kt
@@ -0,0 +1,60 @@
+package com.example.wallet.model.response.transactions
+
+data class AllTransactionWalletsResponse(
+    val _embedded: Embedded,
+    val _links: LinksX,
+    val page: Page
+)
+
+data class WalletX(
+    val href: String
+)
+
+data class Wallet(
+    val _links: Links,
+    val id: Int,
+    val walletName: String
+)
+
+data class SelfX(
+    val href: String
+)
+
+data class Self(
+    val href: String
+)
+
+data class Profile(
+    val href: String
+)
+
+data class Page(
+    val number: Int,
+    val size: Int,
+    val totalElements: Int,
+    val totalPages: Int
+)
+
+data class LinksX(
+    val profile: Profile,
+    val self: SelfX
+)
+
+data class Links(
+    val currency: Currency,
+    val expanses: Expanses,
+    val self: Self,
+    val wallet: WalletX
+)
+
+data class Expanses(
+    val href: String
+)
+
+data class Embedded(
+    val wallets: List<Wallet>
+)
+
+data class Currency(
+    val href: String
+)
