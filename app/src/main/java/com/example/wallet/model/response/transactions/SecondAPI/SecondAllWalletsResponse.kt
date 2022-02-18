94e40a7c1c9627ab2c017f6e3b3518dc08a3e20b
danilashtefan <danilashtefan@gmail.com>
2022-02-18 21:19:13 +0200

    Before wallet change working state

---
+++ app/src/main/java/com/example/wallet/model/response/transactions/SecondAPI/SecondAllWalletsResponse.kt
@@ -0,0 +1,10 @@
+package com.example.wallet.model.response.transactions.SecondAPI
+
+class SecondAllWalletsResponse : ArrayList<SecondAllWalletsResponseItem>()
+
+data class SecondAllWalletsResponseItem(
+    val currency: String,
+    val id: Int,
+    val username: String,
+    val walletName: String
+)
--- app/src/main/java/com/example/wallet/ui/screens/ExpanseCategoriesScreen.kt
+++ app/src/main/java/com/example/wallet/ui/screens/ExpanseCategoriesScreen.kt
@@ -2,11 +2,19 @@ package com.example.wallet.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,11 +35,18 @@ fun ExpanseCategoriesScreen(
     val expanseCategories = viewModel.expanseCategoriesState.value
     val accessToken = viewModel.accessToken.value
 
-    Column() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         LazyColumn{
             items(expanseCategories){expanseCategory->
                 Text(text = "Access token: $accessToken")
             }
         }
+
     }
+
+
 }
+
+
+
+
